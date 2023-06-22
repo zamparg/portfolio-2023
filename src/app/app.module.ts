19cde1b20d14e9848aa683f3d84e2844dbc23ea0
zamparg <zamparg@hotmail.com>
2023-06-22 10:40:18 -0300

    prime commit - estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { QuiensoyComponent } from './components/quiensoy/quiensoy.component';
+import { TrabajosComponent } from './components/trabajos/trabajos.component';
+import { DescripcionComponent } from './components/descripcion/descripcion.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuiensoyComponent,
+    TrabajosComponent,
+    DescripcionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
